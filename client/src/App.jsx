a43cd282df1c93af6e302849c8e3a87eb8c0deda
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-11-04 12:07:33 +0400

    Install React and Tailwind CSS And Create The First Template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import SignUpPage from "./Pages/SignUpPage";
+import SignInPage from "./Pages/SignInPage";
+import AdminPage from "./Pages/AdminPage";
+import CartPage from "./Pages/CartPage";
+import CategoryPage from "./Pages/CategoryPage";
+import PurchaseSucessPage from "./Pages/PurchaseSucessPage";
+import PurchaseCancelPage from "./Pages/PurchaseCancelPage";
+import Navbar from "./components/Navbar";
+import { Toaster } from "react-hot-toast";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen relative overflow-hidden bg-gray-800 text-white">
+      {/* Background */}
+      <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0">
+          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      <div className="relative pt-20 z-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/secret-dashboard" element={<AdminPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/category/:category" element={<CategoryPage />} />
+          <Route path="/purchase-success" element={<PurchaseSucessPage />} />
+          <Route path="/purchase-cancel" element={<PurchaseCancelPage />} />
+        </Routes>
+      </div>
+      <Toaster />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/Pages/PurchaseSucessPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PurchaseSucessPage = () => {
+  return (
+    <div>PurchaseSucessPage</div>
+  )
+}
+
+export default PurchaseSucessPage
